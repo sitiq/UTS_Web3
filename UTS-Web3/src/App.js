4543dbebf7fbe6d92762ca79685540ad15c0723e
sitiqotijah <sitiqotijah01@gmail.com>
2020-04-15 23:08:04 +0700

    hapus

--- UTS-Web3/src/App.js
+++ UTS-Web3/src/App.js
@@ -1,24 +1,22 @@
-import React from 'react';
-import './App.css'
+import React from "react";
 import {
   BrowserRouter,
+  Route,
   Switch,
-}from "react-router-dom";
-
+} from "react-router-dom";
 import Header from "./Header";
 import Blog from "./Blog";
-import BlogDetail from "./BlogDetail";
+import BlogDetail from "./BlogDetail"
 
 //install bootstrap dulu
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import { render } from "@testing-library/react";
 
 // Ini merupakan functional component
 // https://getbootstrap.com/docs/4.4/examples/navbar-static/
 function App() {
   return (
-    
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <div className="App">
         <Header />
         <Switch>
